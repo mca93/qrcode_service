66e01ab2fd067b7fb07c8cfeb16edf604df2b5f4
mca93 <muarucha.assane@gmail.com>
2025-04-12 16:49:44 +0200

    registering client_app routes

--- routes/client_app_routes.go
+++ routes/client_app_routes.go
@@ -9,6 +9,9 @@ func RegisterClientAppRoutes(router *gin.Engine) {
 	v1 := router.Group("/v1")
 	{
 		v1.POST("/clientapps", controllers.CreateClientApp)
+		v1.GET("/clientapps", controllers.ListClientApps)
+		v1.GET("/clientapps/:id", controllers.GetClientApp)    // Obtenha um ClientApp específico pelo ID
+		v1.PUT("/clientapps/:id", controllers.UpdateClientApp) // Atualize um ClientApp específico pelo ID
 		// outras rotas podem ser adicionadas aqui
 	}
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,27 +1,28 @@
 package routes
 
 import (
-	"github.com/mca93/qrcode_service/controllers"
-	"github.com/mca93/qrcode_service/middleware"
-
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(router *gin.Engine) {
 
-	v1 := router.Group("/v1")
-	{
-		v1.GET("/clientapps", controllers.ListClientApps)
-		//v1.POST("/clientapps", controllers.CreateClientApp)
-		v1.GET("/qrcodes/:id/image", controllers.GetQRCodeImage)
-		v1.GET("/clientapps/:clientAppId/apikeys", controllers.ListApiKeys)
-		v1.POST("/clientapps/:clientAppId/apikeys", controllers.CreateApiKey)
+	// v1 := router.Group("/v1")
+	// {
+	// v1.GET("/clientapps", controllers.ListClientApps)
+	// v1.POST("/clientapps", controllers.CreateClientApp)
 
-		v1.GET("/qrcodes", middleware.ApiKeyAuthMiddleware(), controllers.ListQRCodes)
-		v1.POST("/qrcodes", middleware.ApiKeyAuthMiddleware(), controllers.CreateQRCode)
+	// Regista as rotas do ClientApp
+	RegisterClientAppRoutes(router)
 
-		v1.GET("/templates", middleware.ApiKeyAuthMiddleware(), controllers.ListTemplates)
-		v1.POST("/templates", middleware.ApiKeyAuthMiddleware(), controllers.CreateTemplate)
-	}
+	// v1.GET("/qrcodes/:id/image", controllers.GetQRCodeImage)
+	// v1.GET("/clientapps/:clientAppId/apikeys", controllers.ListApiKeys)
+	// v1.POST("/clientapps/:clientAppId/apikeys", controllers.CreateApiKey)
+
+	// v1.GET("/qrcodes", middleware.ApiKeyAuthMiddleware(), controllers.ListQRCodes)
+	// v1.POST("/qrcodes", middleware.ApiKeyAuthMiddleware(), controllers.CreateQRCode)
+
+	// v1.GET("/templates", middleware.ApiKeyAuthMiddleware(), controllers.ListTemplates)
+	// v1.POST("/templates", middleware.ApiKeyAuthMiddleware(), controllers.CreateTemplate)
+	//}
 
 }
