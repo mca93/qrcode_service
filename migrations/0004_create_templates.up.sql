e7057356a4b6457f310600b4182f0ec013392a48
mca93 <muarucha.assane@gmail.com>
2025-04-14 09:13:33 +0200

    removed migrations and frontend folder

--- migrations/0004_create_templates.up.sql
+++
@@ -1,7 +0,0 @@
--- CREATE TABLE templates (
---     id UUID PRIMARY KEY,
---     client_app_id UUID REFERENCES client_apps(id),
---     name TEXT,
---     description TEXT,
---     active BOOLEAN DEFAULT TRUE
--- );
