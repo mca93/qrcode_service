ba442f19bd19a5ed6ae1632d6d687fc2f1fa71f5
mca93 <muarucha.assane@gmail.com>
2025-04-14 14:26:35 +0200

    added JSONMAP interface

--- models/template.go
+++ models/template.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
 	"time"
 )
 
@@ -30,15 +33,15 @@ type QRCodeStyle struct {
 }
 
 type Template struct {
-	ID          string                 `gorm:"primaryKey" json:"id"`
-	Name        string                 `json:"name"`
-	Description string                 `json:"description"`
-	ClientAppID string                 `gorm:"not null" json:"clientAppId"`                   // Foreign key to ClientApp
-	ClientApp   ClientApp              `gorm:"foreignKey:ClientAppID;references:ID" json:"-"` // Association with ClientApp
-	Style       map[string]interface{} `gorm:"type:json" json:"style"`                        // JSON field
-	Active      bool                   `json:"active"`
-	CreatedAt   time.Time              `json:"createdAt"`
-	UpdatedAt   time.Time              `json:"updatedAt"`
+	ID          string    `gorm:"primaryKey" json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	ClientAppID string    `gorm:"not null" json:"clientAppId"`                   // Foreign key to ClientApp
+	ClientApp   ClientApp `gorm:"foreignKey:ClientAppID;references:ID" json:"-"` // Association with ClientApp
+	Style       JSONMap   `gorm:"type:json" json:"style"`                        // JSON field
+	Active      bool      `json:"active"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
 }
 
 type TemplateCreateRequest struct {
@@ -52,4 +55,33 @@ type TemplateUpdateRequest struct {
 	Name        string       `json:"name"`
 	Description string       `json:"description"`
 	Style       *QRCodeStyle `json:"style"`
+}
+
+// JSONMap is a custom type to handle JSON fields in the database.
+type JSONMap map[string]interface{}
+
+// Scan implements the sql.Scanner interface for JSONMap.
+func (j *JSONMap) Scan(value interface{}) error {
+	if value == nil {
+		*j = nil
+		return nil
+	}
+
+	bytes, ok := value.([]uint8)
+	if !ok {
+		return errors.New("failed to scan JSONMap: type assertion to []uint8 failed")
+	}
+
+	if err := json.Unmarshal(bytes, j); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Value implements the driver.Valuer interface for JSONMap.
+func (j JSONMap) Value() (driver.Value, error) {
+	if j == nil {
+		return nil, nil
+	}
+	return json.Marshal(j)
 }
